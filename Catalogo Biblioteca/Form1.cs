ab6b7a812d287c839bc63fce3fe1d0102ee10ea3
Your Name <nikaonob91@gmail.com>
2021-11-18 13:07:59 -0300

    Agregar archivos de proyecto.

---
+++ Catalogo Biblioteca/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Catalogo_Biblioteca.Modelo;
+
+namespace Catalogo_Biblioteca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void CargaDatos()
+        {
+            using (BibliotecaEntities db = new BibliotecaEntities())
+            {
+                var lst = from libros in db.Catalogo select libros;
+                dtg_Libros.DataSource = lst.ToList();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargaDatos();
+        }
+    }
+}
